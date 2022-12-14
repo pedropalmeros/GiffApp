be021160318a9fcdaadc916a0380f279574ee5cf
pedropalmeros <pedrofpalmeros@gmail.com>
2022-12-14 07:46:51 -0600

    [Add] A new component has been created to handle the anime categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 
 export const GifExpertApp = () => {
@@ -7,15 +8,12 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
 
     const onAddCategory = (newCategory) => {
-        console.log(`New category received from child: ${newCategory}`);
-        //setCategories(['Valorant',...categories])
+        console.log(`New category received from child: ${newCategory}`); 
+        if( categories.includes(newCategory)) return;
         setCategories([newCategory,...categories])
     }
     
-
-
-
-  return (
+    return (
     <>
         {/* titulo */}
         <h1>GifExpertApp</h1>
@@ -27,9 +25,13 @@ export const GifExpertApp = () => {
 
         {/* Listado de Gif */}
         <ol>
-            {categories.map((category)=>{
-                return <li key={category}>{category}</li>
-            })}
+            {
+                categories.map((category) => (
+                    <GifGrid 
+                        key={ category } 
+                        category = { category }/> 
+                ) )
+            }
         </ol>
 
         
